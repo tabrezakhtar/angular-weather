1da6fe3e39022c83d250e5ac62a9aea9164673e6
tabrezakhtar <takhtar@brightsolid.com>
2014-05-28 21:33:13 +0100

    added call to openweathermap api

--- app/scripts/controllers/weather-controller.js
+++ app/scripts/controllers/weather-controller.js
@@ -4,4 +4,9 @@ var app = angular.module('angularWeatherApp');
 
 app.controller('WeatherCtrl', ['$scope', 'weatherService', function ($scope, weatherService) {
     $scope.weather = weatherService.getTodaysWeather();
+	$scope.weatherApiResult = '';
+	
+	weatherService.getWeatherApi().then(function(data){
+		$scope.weatherApiResult = data;
+	});
 }]);
--- app/scripts/services/weather-service.js
+++ app/scripts/services/weather-service.js
@@ -2,10 +2,33 @@
 
 var app = angular.module('angularWeatherApp');
 
-app.service('weatherService', function weatherService() {
+app.service('weatherService', function weatherService($http, $q) {
     return {
 	    getTodaysWeather: function () {
 		    return "sunny";
-	    }
+	    },
+		
+		getWeatherApi: function () {
+			var deferred = $q.defer();
+		
+			$http({method: 'GET', url: 'http://api.openweathermap.org/data/2.5/weather?q=London,uk'}).
+				success(function(data, status, headers, config) {
+					deferred.resolve(data);
+				}).
+				error(function(data, status, headers, config) {
+					deferred.reject(data);
+				});
+			
+			return deferred.promise;
+		}
+		
+		//http://openweathermap.org/API
+		//Calls by city name 
+		//api.openweathermap.org/data/2.5/weather?q=London,uk   
+		//Calls by geographic coordinats 
+		//api.openweathermap.org/data/2.5/weather?lat=35&lon=139
+		//Calls by city ID 
+		//api.openweathermap.org/data/2.5/weather?id=2172797
+
     };
 });
